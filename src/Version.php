3803c3e8c20f7f5fc551a9795bfee5c6726c10ea
michael-grunder <michael.grunder@gmail.com>
2025-12-05 12:09:02 -0800

    Update .gitignore and add our version helper

---
+++ src/Version.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mike\Shsuggest;
+
+/**
+ * Central place for the project version string so it can be reused by build tooling later.
+ */
+final class Version
+{
+    public const CURRENT = '0.1.0-dev';
+}
